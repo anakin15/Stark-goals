156cbd3ce54fd8edda1063f2f041e3844c8fc89e
anakin15 <padiwaypadilla@gmail.com>
2025-07-30 16:47:59 -0500

    Create style.cs

---
+++ static/style.cs
@@ -0,0 +1,17 @@
+body {
+    background: #111;
+    color: #fff;
+    text-align: center;
+    font-family: Arial;
+}
+.container {
+    margin-top: 100px;
+}
+.boton {
+    background: #00e676;
+    padding: 10px 20px;
+    color: #000;
+    font-weight: bold;
+    text-decoration: none;
+    border-radius: 8px;
+}
